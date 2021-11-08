33419efec046558f9fef5d13ae1ef51fb04a78f0
Bastien <bastien.pil01@gmail.com>
2021-11-08 17:48:46 +0100

    //Create new project

---
+++ cmake-build-debug/CMakeFiles/Labo05_C_MCRM_PB.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labo05_C_MCRM_PB.dir/main.cpp.obj"
+  "Labo05_C_MCRM_PB.exe"
+  "Labo05_C_MCRM_PB.exe.manifest"
+  "Labo05_C_MCRM_PB.pdb"
+  "libLabo05_C_MCRM_PB.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Labo05_C_MCRM_PB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Labo05_C_MCRM_PB.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Labo05_C_MCRM_PB.
+# This may be replaced when dependencies are built.
